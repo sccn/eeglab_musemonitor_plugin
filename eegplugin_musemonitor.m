8929d6fda15ace8186fd253b54af64c7eb71ab6e
arno <>
2018-07-24 16:58:08 -0700

    initial revision

---
+++ eegplugin_musemonitor.m
@@ -0,0 +1,56 @@
+% eegplugin_musemonitor() - EEGLAB plugin for importing data saved
+%             by the muse monitor Android and iOS app.
+%
+% Usage:
+%   >> eegplugin_musemonitor(fig, trystrs, catchstrs);
+%
+% Inputs:
+%   fig        - [integer]  EEGLAB figure
+%   trystrs    - [struct] "try" strings for menu callbacks.
+%   catchstrs  - [struct] "catch" strings for menu callbacks.
+%
+% Author: Arnaud Delorme, SCC, INC, UCSD
+
+% Copyright (C) 2017 Arnaud Delorme
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+function vers = eegplugin_musemonitor(fig, trystrs, catchstrs)
+
+    vers = 'muse_monitor_1.0';
+    if nargin < 3
+        error('eegplugin_musemonitor requires 3 arguments');
+    end;
+    
+    % add folder to path
+    % ------------------
+    p = which('eegplugin_musemonitor.m');
+    p = p(1:findstr(p,'eegplugin_musemonitor.m')-1);
+    if ~exist('eegplugin_musemonitor')
+        addpath( p );
+    end;
+    
+    % find import data menu
+    % ---------------------
+    menui = findobj(fig, 'tag', 'import data');
+    
+    % menu callbacks
+    % --------------
+    comcnt = [ trystrs.no_check '[EEGTMP LASTCOM] = pop_musemonitor;'  catchstrs.new_non_empty ];
+                
+    % create menus
+    % ------------
+    uimenu( menui, 'label', 'From Muse Monitor App .CSV file', 'separator', 'on', 'callback', comcnt);
---
+++ pop_musemonitor.m
@@ -0,0 +1,135 @@
+% pop_musemonitor() - import data from Muse Monitor Android or iOS app
+%
+% Usage:
+%   >> [EEG, com] = pop_musemonitor; % pop-up window mode
+%   >> [EEG, com] = pop_musemonitor(filename);
+%
+% Optional inputs:
+%   filename  - name of Muse Monitor .csv file
+%
+% Outputs:
+%   EEG       - EEGLAB EEG structure
+%   com       - history string
+%
+% Author: Arnaud Delorme, 2017-
+
+% Copyright (C) 2017 Arnaud Delorme, arno@ucsd.edu
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+% $Id: pop_loadbv.m 53 2010-05-22 21:57:38Z arnodelorme $
+% Revision 1.5 2010/03/23 21:19:52 roy
+% added some lines so that the function can deal with the space lines in the ASCII multiplexed data file
+
+function [EEG, com] = pop_musemonitor(fileName, varargin)
+
+com = '';
+EEG = [];
+
+if nargin < 1
+    [fileName, filePath] = uigetfile2({ '*.csv' '*.CSV' }, 'Select Muse Monitor .csv file - pop_musemonitor()');
+    if fileName(1) == 0, return; end
+    fileName = fullfile(filePath, fileName);
+    
+    promptstr    = { { 'style'  'checkbox'  'string' 'Import auxilary channel' 'tag' 'aux' 'value' 0 } ...
+                     { 'style'  'checkbox'  'string' 'Import power values'     'tag' 'power' 'value' 0 } ...
+                     { 'style'  'checkbox'  'string' 'Import accelerometer (and gyro) values' 'tag' 'acc' 'value' 0 } ...
+                     { 'style'  'checkbox'  'string' 'Import everything' 'tag' 'importall' 'value' 0 } };
+    geometry = { [1] [1] [1] [1] };
+
+    [~,~,~,res] = inputgui( 'geometry', geometry, 'uilist', promptstr, 'helpcom', 'pophelp(''pop_musemonitor'')', 'title', 'Import muse monitor data -- pop_musemonitor()');
+    if isempty(res), return; end
+    
+    options = {};
+    if res.aux,       options = { options{:} 'aux' 'on' }; end
+    if res.power,     options = { options{:} 'power' 'on' }; end
+    if res.acc,       options = { options{:} 'acc' 'on' }; end
+    if res.importall, options = { options{:} 'importall' 'on' }; end
+else
+    options = varargin;
+end
+
+opt = finputcheck(options, { 'aux'       'string'    { 'on' 'off' }    'off';
+                             'power'     'string'    { 'on' 'off' }    'off';
+                             'acc'       'string'    { 'on' 'off' }    'off';
+                             'importall' 'string'    { 'on' 'off' }    'off' }, 'pop_importmuse');
+if isstr(opt), error(opt); end
+
+M = importdata(fileName, ',');
+headerNames =  M.textdata(1,:);
+if length(headerNames) == 1, headerNames = strsplit(headerNames{1}, ','); end
+
+% fist column (time stamp is not imported as 0)
+if size(M.data,2) < length(headerNames)-1, headerNames(1)   = []; end
+if size(M.data,2) < length(headerNames)  , headerNames(end) = []; end
+
+EEG = eeg_emptyset;
+if strcmpi(opt.importall, 'on')
+    allChans = 1:length(headerNames);
+else
+    % import channels
+    realChans   = find(cellfun(@(x)~isempty(strmatch('RAW', x)), headerNames));
+    for iChan = 1:length(realChans)
+        headerNames{realChans(iChan)} = headerNames{realChans(iChan)}(5:end);
+    end
+    
+    % import aux
+    auxChans = [];
+    if strcmpi(opt.aux, 'on')
+        auxChans    = find(cellfun(@(x)~isempty(strmatch('AUX', x)), headerNames));
+    end
+
+    % import accelerometer
+    accChans = [];
+    if strcmpi(opt.acc, 'on')
+        accChans    = find(cellfun(@(x)~isempty(strmatch('Accelerometer', x)), headerNames));
+        gyroChans   = find(cellfun(@(x)~isempty(strmatch('Gyro', x)), headerNames));
+        accChans = [ accChans gyroChans];
+    end
+    
+    % power channels
+    powerChans = [];
+    if strcmpi(opt.power, 'on')
+        deltaChans    = find(cellfun(@(x)~isempty(strmatch('Delta', x)), headerNames));
+        thetaChans    = find(cellfun(@(x)~isempty(strmatch('Theta', x)), headerNames));
+        alphaChans    = find(cellfun(@(x)~isempty(strmatch('Alpha', x)), headerNames));
+        betaChans     = find(cellfun(@(x)~isempty(strmatch('Beta' , x)), headerNames));
+        gammaChans    = find(cellfun(@(x)~isempty(strmatch('Gamma', x)), headerNames));
+        powerChans    = [ deltaChans thetaChans alphaChans betaChans gammaChans];
+    end
+    
+    allChans = [ realChans auxChans accChans powerChans];
+end
+
+EEG.chanlocs = struct('labels', headerNames(allChans));
+EEG.data = M.data(:,allChans)';
+
+% should add discontinuity here for all the NaN segments
+nanPos = find(any(isnan(EEG.data)));
+EEG.data(:,any(isnan(EEG.data))) = [];
+
+%EEG.data = bsxfun(@minus, EEG.data, mean(EEG.data,2));
+EEG.pnts   = size(EEG.data,2);
+EEG.nbchan = size(EEG.data,1);
+EEG.xmin = 0;
+EEG.trials = 1;
+EEG.srate = 300;
+EEG = eeg_checkset(EEG);
+
+if isempty(options)
+    com = sprintf('EEG = pop_musemonitor(''%s'');', fileName);
+else
+    com = sprintf('EEG = pop_musemonitor(''%s'', %s);', fileName, vararg2str(options));
+end
